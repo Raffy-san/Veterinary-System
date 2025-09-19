7ced66c3285ba107f1eb2a15a9b90668efdaee45
Rafael Sanoria <rafaelsanoria506@gmail.com>
2025-09-19 15:22:38 +0800

    On branch Raffy  Your branch is up to date with 'origin/Raffy'.  Changes to be committed: 	modified:   admin/client-management.php 	modified:   admin/medical-records.php 	modified:   admin/pet-management.php 	modified:   owner/medical-records.php 	modified:   owner/my-pets.php

--- admin/pet-management.php
+++ admin/pet-management.php
@@ -157,7 +157,7 @@ if (!$admin) {
                     </div>
                 </div>
                 <div id="notesDetails" class="text-sm bg-white p-4 border-green-400 rounded-lg mt-4 w-full"
-                    style="word-break: break-all; white-space: pre-wrap; overflow-wrap: anywhere;">
+                    style="word-break: break-word; white-space: pre-line;">
                     <!-- Additional details will be populated here -->
                 </div>
             </div>
